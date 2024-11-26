14094001ff78086aebb8fc7c305d88933f82dac4
Arya <jackrack040@gmail.com>
2024-11-26 14:10:37 -0500

    updates

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,16 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"  
+
+const MobileNav = () => {
+  return (
+    <div>MobileNav</div>
+  )
+}
+
+export default MobileNav
